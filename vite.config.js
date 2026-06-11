807bb862651273c8588d8b23953f704282ff35f3
haryor0 <152572683+haryor0@users.noreply.github.com>
2026-06-11 04:11:33 -0400

    Change base path in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/tag-pipeline/',
   resolve: {
     alias: { '@': resolve(__dirname, 'src') },
   },
