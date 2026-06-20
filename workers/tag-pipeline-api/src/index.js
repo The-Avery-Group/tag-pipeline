c27b7ec26e059cf25488f6b0b21d8ad8c6e411b5
haryor0 <152572683+haryor0@users.noreply.github.com>
2026-06-20 05:32:30 -0400

    Add '/sam/run-status' endpoint handling

--- workers/tag-pipeline-api/src/index.js
+++ workers/tag-pipeline-api/src/index.js
@@ -77,6 +77,9 @@ export default {
       } else if (path === '/sam/key-status' && req.method === 'GET') {
         response = await handleSAM(req, env)
 
+      } else if (path === '/sam/run-status' && req.method === 'GET') {
+        response = await handleSAM(req, env)
+
       } else if (path === '/sam/search' && req.method === 'GET') {
         response = await handleSAM(req, env)
 
